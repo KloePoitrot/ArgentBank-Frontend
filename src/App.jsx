02874a184f43a44908cf8b5fa8dc9aa144d22df0
unknown <KloePoitrot@hotmail.com>
2024-09-08 19:31:03 +0200

    Fix footer
    Changed main.css's placement

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
 import Header from './components/header/header'
-import "./main.css"
 import Footer from './components/footer/footer'
+import ImgBanner from './components/imgBanner/imgBanner'
+
+import "./css/main.css"
 
 function App() {
 
   return (
     <>
       <Header/>
+      <main>
+        <ImgBanner>
+          <h2 className="sr-only">Promoted Content</h2>
+          <p className="subtitle">No fees.</p>
+          <p className="subtitle">No minimum deposit.</p>
+          <p className="subtitle">High interest rates.</p>
+          <p className="text">Open a savings account with Argent Bank today!</p>
+        </ImgBanner>
+      </main>
       <Footer/>
     </>
   )
